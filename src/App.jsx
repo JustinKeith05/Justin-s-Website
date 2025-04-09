413644a261de6f83b68a0958dc6ed8fbacbb4acb
Justin Keith Caubang <justinkeithcaubang@macbookpro.3521.dhcp.asu.edu>
2025-04-09 11:47:46 -0700

    Fixed loading bar and typewriter effect.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
+import "./index.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
 
   return (
     <div>
-      {/* Remove the callback function if we want the effect to persist */}
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded}/>}
+      {/* Remove the callback function if we want the effect to persist Or remove the (true)*/}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
     </div>
   )
 }
