19792d32c7723a30cda5f62b1c788abb08b431bc
JustinKeith05 <jbcaubang@gmail.com>
2025-09-07 18:47:26 -0700

    Added work experience

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -85,6 +85,9 @@ export const About = () => {
                                 <li>
                                     <strong>Assistant Instructor</strong> - Kumon (Aug 2018 - Present)
                                 </li>
+                                <li>
+                                    <strong>Student Software Engineer</strong> - Food Trucks Association of Canada (Aug 2024 - May 2025)
+                                </li>
                         </ul>
                     </div>
                 </div>         
